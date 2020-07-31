51c725a4f96efda7c0eeb48bb48258bcdc9bc527
Мерзликина Светлана Юрьевна <s_merzlikina92@mail.ru>
2020-07-31 10:35:43 +0300

    Maven

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+        //Test
+        public void testFunc() {
+            Gson gson = new Gson();
+            RequestSpecification request = RestAssured.given();
+            try {
+                Connection conn = DriverManager.getConnection("url", "user", "password");
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+
+
+}
